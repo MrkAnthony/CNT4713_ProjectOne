74c2c92c2e622adb9637c273fc8ce936f64d033a
Mark-Anthony Delva <mdelv065@fiu.edu>
2026-02-10 17:48:31 -0500

    Part A: Connection & Login (Foundation)

--- myftp.py
+++ myftp.py
@@ -9,31 +9,31 @@
 # import socket module
 from socket import *
 import sys  # In order to terminate the program
-#Hi
 
-def quitFTP(clientSocket):
-    # COMPLETE
+
+def sendCommand(socket, command) -> str:
+    command += '\r\n'
+    # encode the command
     dataOut = command.encode("utf-8")
-    clientSocket.sendall(dataOut)
-    dataIn = clientSocket.recv(1024)
-    data = dataIn.decode("utf-8")
-    print(data)
+    # sendout all the bytes through the network
+    socket.sendall(dataOut)
+
+    response = receiveData(socket)
+    return response
 
 
-def sendCommand(socket, command):
-    dataOut = command.encode("utf-8")
-    # Complete
-    return data
-
-
-def receiveData(clientSocket):
+def receiveData(clientSocket) -> str:
     dataIn = clientSocket.recv(1024)
     data = dataIn.decode("utf-8")
     return data
 
 
-# If you use passive mode you may want to use this method but you have to complete it
-# You will not be penalized if you don't
+def quitFTP(clientSocket) -> None:
+    response = sendCommand(clientSocket, "QUIT")
+    print(response)
+
+
+# Adrian
 def modePASV(clientSocket):
     command = "PASV" + "\r\n"
     # Complete
@@ -45,37 +45,72 @@ def modePASV(clientSocket):
     return status, dataSocket
 
 
+# Adrian
+def ftp_list(clientSocket):
+    pass
+
+
+# Adrian
+def ftp_cd(clientSocket, directory):
+    pass
+
+
+# Milan
+def ftp_get(clientSocket, filename):
+    pass
+
+
+# Milan
+def ftp_put(clientSocket, filename):
+    pass
+
+
+# Milan
+def ftp_delete(clientSocket, filename):
+    pass
+
+
 def main():
-    # COMPLETE
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    HOST = sys.argv[1]
+    clientSocket.connect((HOST, 21))
+    dataIn = receiveData(clientSocket)
+
+    status = 0
+    if dataIn.startswith("220"):
+        status = 220
+    else:
+        print("The server isn't ready")
+        sys.exit()
+
     username = input("Enter the username: ")
     password = input("Enter the password: ")
 
-    clientSocket = socket(AF_INET, SOCK_STREAM)  # TCP socket
-    # COMPLETE
+    # Capturing the server's response
+    email_response = sendCommand(clientSocket, "USER " + username)
+    pass_response = sendCommand(clientSocket, "PASS " + password)
 
-    HOST = # COMPLETE
-    # COMPLETE
-
-    dataIn = receiveData(clientSocket)
     print(dataIn)
-
-    status = 0
-    if dataIn.startswith(""):
-        status = 220
 
     print("Sending username")
-    # COMPLETE
-    print(dataIn)
-
+    print(email_response)
     print("Sending password")
-    if dataIn.startswith(""):
+
+    # validating the server's response
+    if email_response.startswith("331"):
         status = 331
-    # COMPLETE
-    print(dataIn)
+    else:
+        status = 530
+    print(pass_response)
 
-    if dataIn.startswith(""):
+    if pass_response.startswith("230"):
         status = 230
+    else:
+        status = 530
 
+    # Command loop needs to be finished up
+    # will only run if the login was successful with a status code of 230
+    '''
     if status == 230:
         # It is your choice whether to use ACTIVE or PASV mode. In any event:
         # COMPLETE
@@ -83,10 +118,11 @@ def main():
         if pasvStatus == 227:
             # COMPLETE
             pass
+    '''
 
     print("Disconnecting...")
     clientSocket.close()
-    dataSocket.close()
+    # dataSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
 
 
