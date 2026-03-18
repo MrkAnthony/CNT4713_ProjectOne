e8f9ee4cf497a4e9a09fa91c159da092adf0c055
adrianfr-cs <adrianfranquin1@gmail.com>
2026-03-18 05:54:08 -0400

    Change

--- mydns.py
+++ mydns.py
@@ -19,13 +19,104 @@ def parse_response(data):
     # Return: (answers, authority, additional)
     # Each as a list of (name, type, value) tuples
     # Remember to handle DNS name compression (pointers)
-    pass
+    answers = []
+    authority = []
+    additional = []
+
+    # DNS header is 12 bytes
+    # ID, flags, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT
+    header = struct.unpack("!HHHHHH", data[:12])
+    qdcount = header[2]
+    ancount = header[3]
+    nscount = header[4]
+    arcount = header[5]
+
+    offset = 12
+
+    # skip question section
+    for _ in range(qdcount):
+        _, offset = decode_name(data, offset)
+        offset += 4  # QTYPE (2) + QCLASS (2)
+
+    def parse_rr(offset):
+        name, offset = decode_name(data, offset)
+
+        rtype, rclass, ttl, rdlength = struct.unpack("!HHIH", data[offset:offset + 10])
+        offset += 10
+
+        rdata_offset = offset
+
+        # A record
+        if rtype == 1 and rdlength == 4:
+            ip = socket.inet_ntoa(data[offset:offset + 4])
+            value = ip
+
+        # NS record
+        elif rtype == 2:
+            ns_name, _ = decode_name(data, offset)
+            value = ns_name
+
+        else:
+            value = None
+
+        offset += rdlength
+        return (name, rtype, value), offset
+
+    # Answer section
+    for _ in range(ancount):
+        record, offset = parse_rr(offset)
+        if record[1] == 1:  # only keep A
+            answers.append(record)
+
+    # Authority section
+    for _ in range(nscount):
+        record, offset = parse_rr(offset)
+        if record[1] == 2:  # only keep NS
+            authority.append(record)
+
+    # Additional section
+    for _ in range(arcount):
+        record, offset = parse_rr(offset)
+        if record[1] == 1:  # only keep A
+            additional.append(record)
+
+    return answers, authority, additional
 
 
 def decode_name(data, offset):
     # TODO: Decode a DNS name from the packet, handling pointer compression
     # Returns (name_string, new_offset)
-    pass
+    labels = []
+    jumped = False
+    original_offset = offset
+
+    while True:
+        length = data[offset]
+
+        # pointer compression: first two bits are 11
+        if (length & 0xC0) == 0xC0:
+            pointer = ((length & 0x3F) << 8) | data[offset + 1]
+
+            # if this is the first jump, remember where to continue after name
+            if not jumped:
+                original_offset = offset + 2
+
+            offset = pointer
+            jumped = True
+            continue
+
+        # end of name
+        if length == 0:
+            if not jumped:
+                offset += 1
+                return ".".join(labels), offset
+            else:
+                return ".".join(labels), original_offset
+
+        offset += 1
+        label = data[offset:offset + length].decode("utf-8")
+        labels.append(label)
+        offset += length
 
 
 def send_query(server_ip, packet):
