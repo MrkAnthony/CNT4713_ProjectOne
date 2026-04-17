188ce152a22ad0a79afe02cf82b0dfa5212e3d48
Milan Kolich <mkolich17@gmail.com>
2026-04-17 12:44:22 -0400

    Completed server.py

--- server.py
+++ server.py
@@ -28,62 +28,53 @@ def start_server():
     # --- Create control socket and wait for client ---
     print("Creating server socket")
     control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # TODO: bind control_socket to ('', CONTROL_PORT)
-    # TODO: call control_socket.listen(1)
+    control_socket.bind(("", CONTROL_PORT))
+    control_socket.listen(1)
     print("Awaiting connections...")
 
-    # TODO: accept a connection from control_socket
-    # conn, addr = ???
+    conn, addr = control_socket.accept()
     print("Connection requested. Creating data socket")
 
-    # TODO: send DATA_PORT back to the client
-    # conn.sendall(???)
+    conn.sendall(str(DATA_PORT).encode())
 
     # --- Create data socket and wait for client ---
     data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # TODO: bind data_socket to ('', DATA_PORT)
-    # TODO: call data_socket.listen(1)
-    # TODO: accept a connection from data_socket
-    # data_conn, _ = ???
+    data_socket.bind(("", DATA_PORT))
+    data_socket.listen(1)
+    data_conn, _ = data_socket.accept()
 
     # --- Handle tunnel command ---
-    # TODO: receive the "tunnel" command from conn
-    # command = conn.recv(BUFFER_SIZE).decode()
+    command = conn.recv(BUFFER_SIZE).decode()
     print("Tunnel requested. Sending public key")
 
-    # TODO: receive the client's public key bytes from data_conn
-    # client_pub_key_bytes = data_conn.recv(BUFFER_SIZE)
-    # TODO: deserialize it
-    # client_public_key = deserialize_public_key(client_pub_key_bytes)
+    client_pub_key_bytes = data_conn.recv(BUFFER_SIZE)
+    client_public_key = deserialize_public_key(client_pub_key_bytes)
 
-    # TODO: serialize the server's public key and send it on data_conn
-    # server_pub_key_bytes = serialize_public_key(public_key)
-    # data_conn.sendall(server_pub_key_bytes)
+    server_pub_key_bytes = serialize_public_key(public_key)
+    data_conn.sendall(server_pub_key_bytes)
 
     # --- Handle post command ---
-    # TODO: receive the "post" command from conn
-    # command = conn.recv(BUFFER_SIZE).decode()
+    command = conn.recv(BUFFER_SIZE).decode()
     print("Post requested.")
 
-    # TODO: receive the encrypted message from data_conn
-    # encrypted_msg = data_conn.recv(BUFFER_SIZE)
-    # print(f"Received encrypted message: {encrypted_msg}")
+    encrypted_msg = data_conn.recv(BUFFER_SIZE)
+    print(f"Received encrypted message: {encrypted_msg}")
 
-    # TODO: decrypt the message using the server's private_key
-    # decrypted_msg = decrypt_message(encrypted_msg, private_key)
-    # print(f"Decrypted message: {decrypted_msg}")
+    decrypted_msg = decrypt_message(encrypted_msg, private_key)
+    print(f"Decrypted message: {decrypted_msg}")
 
-    # TODO: compute the SHA256 hash of the decrypted message
     print("Computing hash")
-    # msg_hash = compute_sha256(decrypted_msg)
-    # print(f"Responding with hash: {msg_hash}")
+    msg_hash = compute_sha256(decrypted_msg)
+    print(f"Responding with hash: {msg_hash}")
 
-    # TODO: encrypt the hash using the client's public key and send on data_conn
-    # encrypted_hash = encrypt_message(msg_hash, client_public_key)
-    # data_conn.sendall(encrypted_hash)
+    encrypted_hash = encrypt_message(msg_hash, client_public_key)
+    data_conn.sendall(encrypted_hash)
 
     # --- Cleanup ---
-    # TODO: close all sockets
+    data_conn.close()
+    data_socket.close()
+    conn.close()
+    control_socket.close()
 
 
 if __name__ == "__main__":
